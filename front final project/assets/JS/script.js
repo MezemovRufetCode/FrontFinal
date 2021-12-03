ad659a4d84ef45dd134b22e42bc8f8c19620783a
MezemovRufetCode <90691159+MezemovRufetCode@users.noreply.github.com>
2021-12-03 17:02:20 +0400

    Add files via upload

--- front final project/assets/JS/script.js
+++ front final project/assets/JS/script.js
@@ -6,3 +6,14 @@ $(".menuIcon i").click(function () {
 $(".close i").click(function () {
   $(".menu").removeClass("active");
 });
+
+$(function () {
+  $(".nav-links ul li").hover(
+    function () {
+      $(">ul.sub:not(:animated)", this).slideDown(500);
+    },
+    function () {
+      $(">ul.sub", this).slideUp(300);
+    }
+  );
+});
