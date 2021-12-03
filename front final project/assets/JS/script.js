5f734525132223cf490f404a7abe65a4477085ce
MezemovRufetCode <90691159+MezemovRufetCode@users.noreply.github.com>
2021-12-03 19:50:43 +0400

    Add files via upload

--- front final project/assets/JS/script.js
+++ front final project/assets/JS/script.js
@@ -1,19 +1,27 @@
-// let menu = document.querySelector(".menu");
-// let barbtn = document.querySelector(".menuIcon");
-$(".menuIcon i").click(function () {
-  $(".menu").addClass("active");
-});
-$(".close i").click(function () {
-  $(".menu").removeClass("active");
-});
+$(document).ready(function () {
+  $(".menuIcon i").click(function () {
+    $(".menu").addClass("active");
+  });
+  $(".close i").click(function () {
+    $(".menu").removeClass("active");
+  });
 
-$(function () {
-  $(".nav-links ul li").hover(
-    function () {
-      $(">ul.sub:not(:animated)", this).slideDown(500);
-    },
-    function () {
-      $(">ul.sub", this).slideUp(300);
-    }
-  );
+  $(function () {
+    $(".nav-links ul li").hover(
+      function () {
+        $(">ul.sub:not(:animated)", this).slideDown(500);
+      },
+      function () {
+        $(">ul.sub", this).slideUp(150);
+      }
+    );
+  });
+
+  $(".menu-links .sub").hide();
+  $(".mlist").click(function () {
+    $(this).find(".rightIcn").toggleClass("ndeg udeg");
+    // let next = $(this).next();
+    // $(".answer").not(next).slideUp();
+    $(this).next(".sub").slideToggle(200);
+  });
 });
